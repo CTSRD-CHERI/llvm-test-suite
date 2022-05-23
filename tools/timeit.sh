43ff2a4b59a1aa59dbb6a017d9c22e96ba8e2aa3
Alex Richardson <alexrichardson@google.com>
2022-05-23 17:13:24 +0000

    [UPSTREAM] timeit.sh: Handle more of the timeit arguments
    This adds support for the ulimit arguments that the test suite passes.
    The if/else sequnce was also changed to a case to make it easier to update.

--- tools/timeit.sh
+++ tools/timeit.sh
@@ -8,24 +8,53 @@ ERRPUT=/dev/stderr
 PERFSTAT=perfstats
 APPEND_STATUS=0
 
-while [[ $1 = -* ]]; do
-  if [ $1 = "--summary" ]; then
+while :; do
+  case $1 in
+  --summary)
     REPORT=$2
     PERFSTAT="$REPORT.perfstats"
-  elif [ $1 = "--redirect-input" ]; then
+    ;;
+  --redirect-input)
     INPUT=$2
-  elif [ $1 = "--redirect-output" ]; then
+    ;;
+  --redirect-output)
     OUTPUT=$2
     ERRPUT=$2
-  elif [ $1 = "--redirect-stdout" ]; then
+    ;;
+  --redirect-stdout)
     OUTPUT=$2
-  elif [ $1 = "--chdir" ]; then
-    cd $2
-  elif [ $1 = "--append-exitstatus" ]; then
+    ;;
+  --chdir)
+    cd $2 || exit 1
+    ;;
+  --append-exitstatus)
     APPEND_STATUS=1
     shift 1
     continue
-  fi
+    ;;
+  --limit-core)
+    ulimit -c "$2"
+    ;;
+  --limit-cpu)
+    ulimit -t "$2"
+    ;;
+  --limit-file-size)
+    ulimit -f "$2"
+    ;;
+  --limit-rss-size)
+    ulimit -m "$2"
+    ;;
+  --timeout)
+    # Not implemented in timeit.sh
+    ;;
+  -*)
+    echo "Error: unknown command line option $1 $2"
+    exit 1
+    ;;
+  *)
+    break
+    ;;
+  esac
   shift 2
 done
 
@@ -38,14 +67,14 @@ fi
 
 EXITCODE=$?
 if [ "$APPEND_STATUS" = "1" ]; then
-  echo "exit $EXITCODE" >> $OUTPUT
+  echo "exit $EXITCODE" >> "$OUTPUT"
   # When --append-exitstatus is used, timeit will append the
   # exit status to the output file and return 0. Model this
   # behaviour here.
   EXITCODE=0
 fi
 
-echo exit $EXITCODE > $REPORT
+echo exit $EXITCODE > "$REPORT"
 awk -F' ' '{if ($2 ~ /^task-clock/ || $3 ~ /^task-clock/) {gsub(/,/,"",$1);print "user",$1/1000;} else if ($2 == "seconds" && $4 == "elapsed") {print "real",$1;} else if ($2 ~ /^instructions/) {gsub(/,/,"",$1);print "instructions",$1}}' $PERFSTAT >> $REPORT
 
 exit $EXITCODE
