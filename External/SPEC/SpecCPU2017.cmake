ecf2552cdb586a8fe1b3b03d88bc4fbb34532a66
Alex Richardson <alexrichardson@google.com>
2022-11-30 11:12:37 +0000

    [UPSTREAM] Allow building SPEC2017 for FreeBSD
    As with SPEC2006, we can pass -DSPEC_MACOSX to use the appropriate options.

--- External/SPEC/SpecCPU2017.cmake
+++ External/SPEC/SpecCPU2017.cmake
@@ -161,6 +161,9 @@ macro (speccpu2017_benchmark)
       # Windows x86/x64
     elseif (TARGET_OS STREQUAL "Darwin")
       add_definitions(-DSPEC_MACOSX)
+    elseif (TARGET_OS STREQUAL "FreeBSD")
+        # For FreeBSD passing -DSPEC_MACOSX selects the right options.
+        list(APPEND CPPFLAGS -DSPEC_MACOSX)
     else ()
       message("ARCH: ${ARCH}")
       message("TARGET_OS: ${TARGET_OS}")
